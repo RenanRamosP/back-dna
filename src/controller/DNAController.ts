91682eeeb92cf09f8120ef402228f9401eda4d06
Renan Ramos Pereira <renanrp@pop-os.localdomain>
2023-11-17 01:15:34 -0300

    fix(DNA): add error handling on controller

--- src/controller/DNAController.ts
+++ src/controller/DNAController.ts
@@ -7,59 +7,70 @@ export class DNAController {
   private dnaRepository = AppDataSource.getRepository(DNA);
 
   async all(request: Request, response: Response, next: NextFunction) {
-    const allDNAs = await this.dnaRepository.find({
-      relations: { humanType: true },
-    });
+    try {
+      const allDNAs = await this.dnaRepository
+        .createQueryBuilder("DNA")
+        .leftJoinAndSelect("DNA.humanType", "type")
+        .getMany();
 
-    return await this.dnaRepository
-      .createQueryBuilder("DNA")
-      .leftJoinAndSelect("DNA.humanType", "type")
-      .getMany();
+      response.status(200).send(allDNAs);
+    } catch (err) {
+      response.status(400).send(err.message);
+    }
   }
 
   async one(request: Request, response: Response, next: NextFunction) {
-    const id = parseInt(request.params.id);
+    try {
+      if (request.params.id === undefined) throw new Error("ID is required");
+      const id = parseInt(request.params.id);
 
-    const user = await this.dnaRepository.findOne({
-      where: { id },
-    });
+      const DNA = await this.dnaRepository.findOne({
+        where: { id },
+      });
 
-    if (!user) {
-      return "unregistered user";
+      if (!DNA) {
+        throw new Error("DNA not found");
+      }
+      response.status(200).send(DNA);
+    } catch (err) {
+      response.status(400).send(err.message);
     }
-    return user;
   }
 
   async save(request: Request, response: Response, next: NextFunction) {
-    const { dna } = request.body;
-    if (!dna) throw new Error("DNA is required");
-    const matrixSize = Math.sqrt(dna.length);
-    if (matrixSize % 1 !== 0) throw new Error("Invalid DNA size");
-    if (!dna.match(/^[ATCG]+$/)) throw new Error("Invalid DNA sequence");
+    try {
+      const { dna } = request.body;
+      if (!dna) throw new Error("DNA is required");
+      const matrixSize = Math.sqrt(dna.length);
+      if (matrixSize % 1 !== 0) throw new Error("Invalid DNA size");
+      if (!dna.match(/^[ATCG]+$/)) throw new Error("Invalid DNA sequence");
 
-    const existantDNA = await this.dnaRepository
-      .createQueryBuilder("DNA")
-      .leftJoinAndSelect("DNA.humanType", "type")
-      .where("content = :dna", { dna })
-      .getOne();
+      const existantDNA = await this.dnaRepository
+        .createQueryBuilder("DNA")
+        .leftJoinAndSelect("DNA.humanType", "type")
+        .where("content = :dna", { dna })
+        .getOne();
 
-    if (existantDNA) return existantDNA;
+      if (existantDNA) response.status(200).send(existantDNA);
 
-    const splittedDNA = [];
-    for (let i = 0; i < dna.length; i += matrixSize) {
-      splittedDNA.push(dna.slice(i, i + matrixSize));
+      const splittedDNA = [];
+      for (let i = 0; i < dna.length; i += matrixSize) {
+        splittedDNA.push(dna.slice(i, i + matrixSize));
+      }
+
+      let humanType = 1;
+      const isModded = isSigmano(splittedDNA);
+
+      if (isModded) humanType = 2;
+
+      const dnaToSave = Object.assign(new DNA(), {
+        content: dna,
+        humanType,
+      });
+
+      response.status(201).send(dnaToSave);
+    } catch (err) {
+      response.status(400).send(err.message);
     }
-
-    let humanType = 1;
-    const isModded = isSigmano(splittedDNA);
-
-    if (isModded) humanType = 2;
-
-    const dnaToSave = Object.assign(new DNA(), {
-      content: dna,
-      humanType,
-    });
-
-    return this.dnaRepository.save(dnaToSave);
   }
 }
